806b40971e9df84841684f91ce8fedac2c1e0a11
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 16:01:58 -0500

    Fix ffi for text decoder due to usage of new

--- src/Web/Encoding/TextDecoder.js
+++ src/Web/Encoding/TextDecoder.js
@@ -1,8 +1,9 @@
-export function new(label) {
+const newImpl = function (label) {
   return function() {
     return new TextDecoder(label);
   };
-}
+};
+export { newImpl as new };
 
 export function _decode(view, options, decoder) {
   return decoder.decode(view, options);
